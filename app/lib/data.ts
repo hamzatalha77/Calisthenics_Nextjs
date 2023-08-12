6c71bc63830498f268a998cb9b15dbd9f2668cc6
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-12 12:01:44 +0100

    commit

---
+++ app/lib/data.ts
@@ -0,0 +1,15 @@
+type Exercise = {
+  _id: string
+  title: string
+  description: string
+  images: [string]
+  video: string
+  tags: [string]
+  muscles: [string]
+  technique: string
+  reps: number
+  sets: number
+  duration: number
+  category: string
+  subcategories: [string]
+}
