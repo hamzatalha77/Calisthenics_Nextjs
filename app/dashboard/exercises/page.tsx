98e7922235122f1d873d58ec26743164f5b769e6
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-12 14:08:42 +0100

    commit

---
+++ app/dashboard/exercises/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateExerciseScreen = () => {
+  return (
+    <div className="App">
+      <h1>Create Exercise</h1>
+    </div>
+  )
+}
+
+export default CreateExerciseScreen
