39990cd3bfc5f9f2b857f19e5c65faf496e1aad9
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-12 12:18:14 +0100

    commit

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -1,13 +1,21 @@
 import { NextResponse } from 'next/server'
 import { exercises, db } from '../db'
+import { fetchExercises } from '../../lib/data'
 
 type Data = {
   name: string
 }
 
-export async function GET(req: Request) {
-  const data = await exercises.find({}).toArray()
-  return NextResponse.json(data)
+export async function GET(req: Request, res: Response) {
+  try {
+    const exercisesData = await fetchExercises()
+    return NextResponse.json(
+      { message: 'ok', exercises: exercisesData },
+      { status: 201 }
+    )
+  } catch (error) {
+    return NextResponse.json({ message: 'Error', error }, { status: 500 })
+  }
 }
 
 export async function POST(req: Request) {}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,15 +1,29 @@
+import { ObjectId } from 'mongodb'
+import { exercises } from '../api/db'
+
 type Exercise = {
-  _id: string
+  _id: ObjectId
   title: string
   description: string
-  images: [string]
+  images: string[]
   video: string
-  tags: [string]
-  muscles: [string]
+  tags: string[]
+  muscles: string[]
   technique: string
   reps: number
   sets: number
   duration: number
   category: string
-  subcategories: [string]
+  subcategories: string[]
 }
+
+const fetchExercises = async () => {
+  const exerciseDocuments = await exercises.find().toArray()
+  return exerciseDocuments
+}
+
+const createExercise = async (exercise: Exercise) => {
+  await exercises.insertOne(exercise)
+}
+
+export { fetchExercises, createExercise }
