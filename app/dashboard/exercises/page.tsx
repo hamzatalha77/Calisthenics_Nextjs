da1a0290cadabb246c2b3da877bf2aec92c5d77b
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-14 18:49:12 +0100

    commit

--- app/dashboard/exercises/page.tsx
+++ app/dashboard/exercises/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { ComponentPropsWithRef, useState } from 'react'
+import React, { SyntheticEvent, ComponentPropsWithRef, useState } from 'react'
 type Props = ComponentPropsWithRef<'input'>
 
 const CreateExerciseScreen = (props: Props) => {
@@ -16,7 +16,7 @@ const CreateExerciseScreen = (props: Props) => {
   const [duration, setDuration] = useState('')
   const [category, setCategory] = useState('')
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
     fetch('http://localhost:3000/api/exercises', {
       method: 'POST',
@@ -41,7 +41,6 @@ const CreateExerciseScreen = (props: Props) => {
   return (
     <div>
       <h1 className="text-center text-6xl">Create Exercise</h1>
-
       <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20">
         <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
@@ -119,7 +118,6 @@ const CreateExerciseScreen = (props: Props) => {
               <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
                 <option>Surabaya</option>
                 <option>Jakarta</option>
-                <option>Tangerang</option>
                 <option>Bandung</option>
               </select>
             </div>
@@ -223,78 +221,6 @@ const CreateExerciseScreen = (props: Props) => {
           </div>
         </form>
       </section>
-
-      {/* <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-20">
-        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
-          Account settings
-        </h2>
-
-        <form>
-          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-            <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="username"
-              >
-                Username
-              </label>
-              <input
-                id="username"
-                type="text"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-            </div>
-
-            <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="emailAddress"
-              >
-                Email Address
-              </label>
-              <input
-                id="emailAddress"
-                type="email"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-            </div>
-
-            <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                id="password"
-                type="password"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-            </div>
-
-            <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="passwordConfirmation"
-              >
-                Password Confirmation
-              </label>
-              <input
-                id="passwordConfirmation"
-                type="password"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-            </div>
-          </div>
-
-          <div className="flex justify-end mt-6">
-            <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Save
-            </button>
-          </div>
-        </form>
-      </section> */}
     </div>
   )
 }
