5e14146990aa3007422bf0f62861432a2a248f41
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-14 18:47:38 +0100

    COMMIT

--- app/dashboard/exercises/page.tsx
+++ app/dashboard/exercises/page.tsx
@@ -40,12 +40,9 @@ const CreateExerciseScreen = (props: Props) => {
   }
   return (
     <div>
-      <h1>Create Exercise</h1>
+      <h1 className="text-center text-6xl">Create Exercise</h1>
 
       <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20">
-        <h1 className="text-xl font-bold text-white capitalize dark:text-white">
-          Account settings
-        </h1>
         <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
@@ -55,6 +52,7 @@ const CreateExerciseScreen = (props: Props) => {
               <input
                 id="title"
                 type="text"
+                onChange={(e) => setTitle(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
             </div>
@@ -66,6 +64,7 @@ const CreateExerciseScreen = (props: Props) => {
               <input
                 id="video"
                 type="email"
+                onChange={(e) => setVideo(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
             </div>
@@ -80,6 +79,7 @@ const CreateExerciseScreen = (props: Props) => {
               <input
                 id="technique"
                 type="text"
+                onChange={(e) => setTechnique(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
             </div>
@@ -91,6 +91,7 @@ const CreateExerciseScreen = (props: Props) => {
               <input
                 id="tags"
                 type="text"
+                onChange={(e) => setTags(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
             </div>
@@ -104,6 +105,7 @@ const CreateExerciseScreen = (props: Props) => {
               <input
                 id="muscles"
                 type="text"
+                onChange={(e) => setMuscles(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
             </div>
@@ -126,8 +128,9 @@ const CreateExerciseScreen = (props: Props) => {
                 Reps
               </label>
               <input
-                id="muscles"
+                id="Reps"
                 type="number"
+                onChange={(e) => setReps(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
             </div>
@@ -138,6 +141,7 @@ const CreateExerciseScreen = (props: Props) => {
               <input
                 id="sets"
                 type="number"
+                onChange={(e) => setSets(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
             </div>
@@ -151,6 +155,7 @@ const CreateExerciseScreen = (props: Props) => {
               <input
                 id="duration"
                 type="number"
+                onChange={(e) => setDuration(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
             </div>
@@ -163,6 +168,7 @@ const CreateExerciseScreen = (props: Props) => {
               </label>
               <textarea
                 id="textarea"
+                onChange={(e) => setDescription(e.target.value)}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               ></textarea>
             </div>
@@ -181,9 +187,9 @@ const CreateExerciseScreen = (props: Props) => {
                   >
                     <path
                       d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
                   </svg>
                   <div className="flex text-sm text-gray-600">
