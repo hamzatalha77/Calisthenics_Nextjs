f519d1473ff2bf13c75ef91d678631a386f4392c
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-14 17:37:05 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'postcss-flexbugs-fixes',
-    'postcss-preset-env',
-    [
-      'postcss-normalize',
-      {
-        allowDuplicates: false
-      }
-    ],
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}'
-        ],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
-      }
-    ],
-    'autoprefixer'
-  ]
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
 }
