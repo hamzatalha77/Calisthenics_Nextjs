7132ee9f44e0a054a8ae3955247454122ae4fa04
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-27 12:47:33 +0100

    commit

--- app/dashboard/exercises/allExercise/page.tsx
+++ app/dashboard/exercises/allExercise/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 type Exercise = {
   _id: string
@@ -8,75 +8,88 @@ type Exercise = {
   description: string
 }
 
-const getExercises = async (): Promise<Exercise[]> => {
-  const data = await fetch('http://localhost:3000/api/exercises')
-  const exercises = await data.json()
-  return exercises
-}
+const Exercises: React.FC = () => {
+  const [exercises, setExercises] = useState<Exercise[] | null>(null)
 
-export default async function Exercises() {
-  const exercises = await getExercises()
+  useEffect(() => {
+    const fetchExercises = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/exercises')
+        const data = await response.json()
+        console.log('Fetched data:', data)
+        console.log('Data type:', typeof data)
+        setExercises(data)
+      } catch (error) {
+        console.error('Error fetching exercises:', error)
+      }
+    }
+
+    fetchExercises()
+  }, [])
+
+  if (exercises === null) {
+    // You can render a loading indicator here
+    return <div>Loading...</div>
+  }
   return (
-    <div>
-      <div className="max-w-2xl mx-auto">
-        <div className="flex flex-col">
-          <div className="overflow-x-auto shadow-md sm:rounded-lg">
-            <div className="inline-block min-w-full align-middle">
-              <div className="overflow-hidden ">
-                <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                  <thead className="bg-gray-100 dark:bg-gray-700">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
-                      >
-                        Product Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
-                      >
-                        Category
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
-                      >
-                        Price
-                      </th>
-                      <th scope="col" className="p-4">
-                        <span className="sr-only">Edit</span>
-                      </th>
+    <div className="max-w-2xl mx-auto">
+      <div className="flex flex-col">
+        <div className="overflow-x-auto shadow-md sm:rounded-lg">
+          <div className="inline-block min-w-full align-middle">
+            <div className="overflow-hidden ">
+              <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                <thead className="bg-gray-100 dark:bg-gray-700">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                    >
+                      Product Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                    >
+                      Category
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                    >
+                      Price
+                    </th>
+                    <th scope="col" className="p-4">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                  {exercises.map((exercise, index) => (
+                    <tr
+                      className="hover:bg-gray-100 dark:hover:bg-gray-700"
+                      key={exercise._id}
+                    >
+                      <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {exercise.title}
+                      </td>
+                      <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                        {exercise.video}
+                      </td>
+                      <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {exercise.description}
+                      </td>
+                      <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                        <a
+                          href="#"
+                          className="text-blue-600 dark:text-blue-500 hover:underline"
+                        >
+                          Edit
+                        </a>
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                    {exercises.map((exercise, index) => (
-                      <tr
-                        className="hover:bg-gray-100 dark:hover:bg-gray-700"
-                        key={exercise._id}
-                      >
-                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          {exercise.title}
-                        </td>
-                        <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">
-                          {exercise.video}
-                        </td>
-                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          {exercise.description}
-                        </td>
-                        <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                          <a
-                            href="#"
-                            className="text-blue-600 dark:text-blue-500 hover:underline"
-                          >
-                            Edit
-                          </a>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
