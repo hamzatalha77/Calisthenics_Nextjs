a09b80223682fbe5a043eab74fa8db9adcadc6cc
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-27 17:38:41 +0100

    commit

--- app/dashboard/exercises/allExercise/page.tsx
+++ app/dashboard/exercises/allExercise/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react'
 
 type Exercise = {
@@ -56,30 +57,35 @@ export default function ExercisesList() {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                  {exercises.map((exercise, index) => (
-                    <tr
-                      className="hover:bg-gray-100 dark:hover:bg-gray-700"
-                      key={exercise._id}
-                    >
-                      <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {exercise.title}
-                      </td>
-                      <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">
-                        {exercise.video}
-                      </td>
-                      <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {exercise.description}
-                      </td>
-                      <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                        <a
-                          href="#"
-                          className="text-blue-600 dark:text-blue-500 hover:underline"
-                        >
-                          Edit
-                        </a>
-                      </td>
-                    </tr>
-                  ))}
+                  {exercises.length === 0 ? (
+                    // Loading state
+                    <p>Loading...</p>
+                  ) : (
+                    exercises.map((exercise, index) => (
+                      <tr
+                        className="hover:bg-gray-100 dark:hover:bg-gray-700"
+                        key={exercise._id}
+                      >
+                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          {exercise.title}
+                        </td>
+                        <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                          {exercise.video}
+                        </td>
+                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          {exercise.description}
+                        </td>
+                        <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                          <a
+                            href="#"
+                            className="text-blue-600 dark:text-blue-500 hover:underline"
+                          >
+                            Edit
+                          </a>
+                        </td>
+                      </tr>
+                    ))
+                  )}
                 </tbody>
               </table>
             </div>
