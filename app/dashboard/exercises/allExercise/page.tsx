11e0e34743dfec0243990b75d940e25c5894ccde
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-29 20:06:05 +0100

    commit

--- app/dashboard/exercises/allExercise/page.tsx
+++ app/dashboard/exercises/allExercise/page.tsx
@@ -62,41 +62,43 @@ export default function ExercisesList() {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                  {exercises.length === 0 ? (
-                    <p>Loading...</p>
+                  {loading ? (
+                    <div>Loading...</div>
                   ) : (
-                    exercises.map((exercise, index) => (
-                      <tr
-                        className="hover:bg-gray-100 dark:hover:bg-gray-700"
-                        key={exercise._id}
-                      >
-                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          {exercise.title}
-                        </td>
-                        <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">
-                          {exercise.video}
-                        </td>
-                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          {exercise.description}
-                        </td>
-                        <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                          <a
-                            href="#"
-                            className="text-blue-600 dark:text-blue-500 hover:underline"
-                          >
-                            Edit
-                          </a>
-                        </td>
-                        <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                          <a
-                            href="#"
-                            className="text-blue-600 dark:text-blue-500 hover:underline"
-                          >
-                            Delete
-                          </a>
-                        </td>
-                      </tr>
-                    ))
+                    <>
+                      {exercises.map((exercise) => (
+                        <tr
+                          className="hover:bg-gray-100 dark:hover:bg-gray-700"
+                          key={exercise._id}
+                        >
+                          <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {exercise.title}
+                          </td>
+                          <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                            {exercise.video}
+                          </td>
+                          <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {exercise.description}
+                          </td>
+                          <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                            <a
+                              href="#"
+                              className="text-blue-600 dark:text-blue-500 hover:underline"
+                            >
+                              Edit
+                            </a>
+                          </td>
+                          <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                            <a
+                              href="#"
+                              className="text-blue-600 dark:text-blue-500 hover:underline"
+                            >
+                              Delete
+                            </a>
+                          </td>
+                        </tr>
+                      ))}
+                    </>
                   )}
                 </tbody>
               </table>
