eef656bbb23156d7ef189aeb069ef87dc74f9145
hamzatalha77 <hamza_ta7@hotmail.com>
2024-01-26 23:00:26 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
 import React from 'react'
 
 function page() {
-  return <div>Index</div>
+  const { user, isLoading, error } = useUser()
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>Error</div>
+  }
+  if (user) {
+    return (
+      <div>
+        welcome to calisthenics {user.name}!{' '}
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+    )
+  }
 }
 
 export default page
