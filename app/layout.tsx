997ae5cb6bc733c4d0eff29874cc8eef7f628f1d
hamzatalha77 <hamza_ta7@hotmail.com>
2024-01-26 18:55:22 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import PageWrapper from './components/dashboard/page-wrapper'
 import SideNav from './components/dashboard/side-nav'
 import './styles/globals.css'
 import { Inter } from 'next/font/google'
-
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,19 +15,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <div className="flex">
-          <SideNav />
-          <main className="flex-1">
-            <MarginWidthWrapper>
-              <Header />
-              <HeaderMobile />
-              <PageWrapper>{children}</PageWrapper>
-            </MarginWidthWrapper>
-          </main>
-        </div>
-      </body>
-    </html>
+    <UserProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <div className="flex">
+            <SideNav />
+            <main className="flex-1">
+              <MarginWidthWrapper>
+                <Header />
+                <HeaderMobile />
+                <PageWrapper>{children}</PageWrapper>
+              </MarginWidthWrapper>
+            </main>
+          </div>
+        </body>
+      </html>
+    </UserProvider>
   )
 }
