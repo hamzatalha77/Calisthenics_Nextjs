67a321918793babe291daebf120796767a7a4536
hamzatalha77 <hamza_ta7@hotmail.com>
2024-01-28 18:59:44 +0100

    commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import GithubProvider from 'next-auth/providers/github'
 const handler = NextAuth({
   providers: [
     GithubProvider({
-      clientId: '',
-      clientSecret: ''
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET
     })
   ]
 })
