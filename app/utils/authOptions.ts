e10739fbba24275500b2497bb961f8c9a2ed103d
hamzatalha77 <hamza_ta7@hotmail.com>
2024-02-22 11:09:43 +0100

    COMMIT

---
+++ app/utils/authOptions.ts
@@ -0,0 +1,78 @@
+import NextAuth from 'next-auth'
+import { Account, User as AuthUser } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcryptjs'
+import { connectToMongo, users } from '../lib/myconnexion'
+
+export const authOptions: any = {
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials: any): Promise<any | null> {
+        await connectToMongo()
+        try {
+          const user = await users.findOne({ email: credentials.email })
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            )
+            if (isPasswordCorrect) {
+              return user
+            } else {
+              throw new Error('Wrong Credentials!')
+            }
+          } else {
+            throw new Error('User not found!')
+          }
+        } catch (err: any) {
+          throw new Error(err)
+        }
+      }
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? ''
+    })
+  ],
+  callbacks: {
+    async signIn({ user, account }: { user: AuthUser; account: Account }) {
+      if (account?.provider === 'credentials') {
+        return true
+      }
+      if (account?.provider === 'github') {
+        await connectToMongo()
+        try {
+          const existingUser = await users.findOne({ email: user.email })
+          if (!existingUser) {
+            const hashedPassword = await bcrypt.hash('someRandomPassword', 5)
+            const newUser = {
+              email: user.email,
+              password: hashedPassword,
+              username: user.name,
+              avatar: user.image,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            }
+
+            await users.insertOne(newUser)
+            return true
+          }
+          return true
+        } catch (err) {
+          console.log('Error saving user', err)
+          return false
+        }
+      }
+    }
+  }
+}
+
+export const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
